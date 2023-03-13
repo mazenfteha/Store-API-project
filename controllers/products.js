177e5aa6498dfdd4bcd37e69df6f0efb4c1986fa
mazenfteha <mazn34903@gmail.com>
2023-03-12 21:55:04 -0700

    add select fields to our API

--- controllers/products.js
+++ controllers/products.js
@@ -2,13 +2,13 @@ const Product =require('../models/product')
 
 const getAllProductsStatic =async (req,res) =>{
     const search ='a'
-    const products =await Product.find({}).sort('-name')
+    const products =await Product.find({}).select('name price')
     res.status(200).json({ products, nbHits: products.length })
 }
 
 
 const getAllProducts =async (req,res) =>{
-    const { featured , company, name, sort} =req.query
+    const { featured , company, name, sort, fields} =req.query
     const queryObject = {}
 
     if(featured){
@@ -22,12 +22,21 @@ const getAllProducts =async (req,res) =>{
     }
     //console.log(queryObject)
     let result =Product.find(queryObject)
+    //sort
     if(sort){
         const sortList = sort.split(',').join(' ')
         result =result.sort(sortList)
     }else{
         result =result.sort('createdAt')
     }
+
+    //select
+    if(fields){
+        const fieldList =fields.split(',').join('')
+        result = result.select(fieldList)
+    }
+
+
     const products =await result 
     res.status(200).json({products, nbHits: products.length})
 }
