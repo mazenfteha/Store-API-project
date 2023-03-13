eef48843abdbec360b745be549b10f23b9058f31
mazenfteha <mazn34903@gmail.com>
2023-03-12 18:40:23 -0700

    search products based on {property}

--- controllers/products.js
+++ controllers/products.js
@@ -1,12 +1,24 @@
-
+const Product =require('../models/product')
 
 const getAllProductsStatic =async (req,res) =>{
-    res.status(200).json({msg: 'products testing route'})
+    const products =await Product.find({})
+    res.status(200).json({ products, nbHits: products.length })
 }
 
 
 const getAllProducts =async (req,res) =>{
-    res.status(200).json({msg: 'products route'})
+    const { featured , company} =req.query
+    const queryObject = {}
+
+    if(featured){
+        queryObject.featured =featured === 'true'? true : false
+    }
+    if(company){
+        queryObject.company=company
+    }
+    console.log(queryObject)
+    const products =await Product.find(queryObject)
+    res.status(200).json({products, nbHits: products.length})
 }
 
 module.exports ={
