362c88216c0efade469106742ee410455dd60135
mazenfteha <mazn34903@gmail.com>
2023-03-12 22:27:19 -0700

    add limit-skip-page

--- controllers/products.js
+++ controllers/products.js
@@ -2,7 +2,7 @@ const Product =require('../models/product')
 
 const getAllProductsStatic =async (req,res) =>{
     const search ='a'
-    const products =await Product.find({}).select('name price')
+    const products =await Product.find({}).sort('name').select('name price').limit(10).skip(5)
     res.status(200).json({ products, nbHits: products.length })
 }
 
@@ -36,6 +36,11 @@ const getAllProducts =async (req,res) =>{
         result = result.select(fieldList)
     }
 
+    const page =Number(req.query.page) || 1
+    const limit =Number(req.query.limit) || 10
+    const  skip =(page -1) * limit
+
+    result =result.skip(skip).limit(limit)
 
     const products =await result 
     res.status(200).json({products, nbHits: products.length})
